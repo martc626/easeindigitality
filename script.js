7ba2c51f3330d2450059d8c5be4acbc120a60014
martc626 <martc626@newschool.edu>
2021-10-10 17:04:22 -0400

    third cloud

--- script.js
+++ script.js
@@ -30,10 +30,12 @@ const touch = (event, offset) => {
 
 let cloud1 = document.getElementById('cloud-1');
 let cloud2 = document.getElementById('cloud-2');
+let cloud3 = document.getElementById('cloud-3');
 
 window.addEventListener('scroll', function(){
   var value = window.scrollY;
 
   cloud1.style.right = value * 1.5 + 'px';
-  cloud2.style.left = value * 1.5 + 'px';  
+  cloud2.style.left = value * 1.5 + 'px'; 
+  cloud3.style.right = value * 1.5 + 'px';  
 })
