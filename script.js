2bfd458cc342379dfcae2df837b9285e30376b0b
martc626 <martc626@newschool.edu>
2021-10-11 18:34:36 -0400

    moon

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-const floating = (event, offset) => {
-	let mouseX = event.clientX
-	let mouseY = event.clientY
-	let halfWidth = window.innerWidth / 2
-	let halfHeight = window.innerHeight / 2
-	let x = mouseX - halfWidth
-	let y = mouseY - halfHeight
-  	// console.log(x + "," + y)
-  	let  transX = x * -1 * offset / halfWidth
-  	let  transY = y * -1 * offset / halfHeight
-  	// console.log(transX + "," + transY)
-  	let coords = [transX,transY]
-  	return coords	
-}
-
-const touch = (event, offset) => {
-  let mouseX = event.touches[0].clientX
-  let mouseY = event.touches[0].clientY
-  let halfWidth = window.innerWidth / 2
-  let halfHeight = window.innerHeight / 2
-  let x = mouseX - halfWidth
-  let y = mouseY - halfHeight
-  // console.log(x + "," + y)
-  let  transX = x * -1 * offset / halfWidth
-  let  transY = y * -1 * offset / halfHeight
-  // console.log(transX + "," + transY)
-  let coords = [transX,transY]
-  return coords
-}
-
 let cloud1 = document.getElementById('cloud-1');
 let cloud2 = document.getElementById('cloud-2');
 let cloud3 = document.getElementById('cloud-3');
@@ -37,5 +7,5 @@ window.addEventListener('scroll', function(){
 
   cloud1.style.right = value * 1.5 + 'px';
   cloud2.style.left = value * 1.5 + 'px'; 
-  cloud3.style.right = value * 1.5 + 'px';  
+  cloud3.style.right = value * 1.5 + 'px'; 
 })
